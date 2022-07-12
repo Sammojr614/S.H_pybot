10d00fc2f84fdbc1b60eb7bf1d17ba177c548f17
Sammmojr <sammojr614@gmail.com>
2022-07-12 11:31:45 -0400

    made the Help command look better

--- helpCommand.py
+++ helpCommand.py
@@ -1,5 +1,4 @@
 import os
-from click import pass_context
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -13,9 +12,8 @@ class commandHelp(commands.Cog):
         
     @commands.command(pass_context=True)
     async def help(self,ctx):
-        bot = self.bot
-        embed = discord.Embed(title="Help Menu",describe="``The Help list for all of my Commands``",color=0x56B9CD)
-        embed.add_field(name="test", value="Use this command to test things", inline=False)
+        embed = discord.Embed(title="__Help Menu__",color=0x56B9CD)
+        embed.add_field(name="test", value="``Use this command to test things``", inline=False)
         embed.add_field(name="randNumb [value]", value="``Random Number Generator with [value] being the max``", inline=False)
         embed.set_footer(text=f"Ver {ver}")
         await ctx.send(embed=embed)
