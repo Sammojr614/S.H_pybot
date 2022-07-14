4da959ea5639d7c6848b9d424935e6ae8ec5dd68
Sammmojr <sammojr614@gmail.com>
2022-07-14 16:37:15 -0400

    Working on the sprint Command

--- sprints.py
+++ sprints.py
@@ -1,17 +1,26 @@
-from click import pass_context
+
 import discord
 from discord.ext import commands
-import io
-import sys
+import os
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ver = os.getenv("versionNumb")
 
 class saveSprint(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command(pass_context = True)
-    async def sprint(self,ctx, args):
-        if args.lower() == "current":
-            file = open("sprints.txt")
-            await ctx.message.delete()
-            await ctx.send(file.read())
+    @commands.command()
+    async def sprint(self,ctx):
+        file = open("sprints.txt")
+        await ctx.message.delete()
+        sprintEmbed = discord.Embed(title="__Sprint Schedule__",discription="A Reminder of Our Sprint Schedule", color=0x56B9CD)
+        sprintEmbed.add_field(name="Sprint",value="```" + file.read() +"```")
+        sprintEmbed.set_footer(ver)
+        await ctx.send(embed=sprintEmbed)
+            
             
 
