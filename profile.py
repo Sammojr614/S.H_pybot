235df1f34248832c54d9a460362bb862f820a3a5
Sammojr614 <Sammojr614@gmail.com>
2022-08-03 14:32:27 -0400

    Profiles are somewhat up and running

--- profile.py
+++ profile.py
@@ -5,6 +5,9 @@ from discord import Embed
 from dotenv import load_dotenv
 import os
 import json
+import stat
+
+
 
 load_dotenv()
 
@@ -14,35 +17,47 @@ ver = os.getenv("Version")
 class makeProfile(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-
+        self.devList = []
     @commands.command(pass_context=True)
     async def profile(self,ctx,*args):
-        profileEmbed = Embed(title="Profile",discription="Your Work Profile",color=0x56B9CD)
-        profileEmbed.set_footer(text=ver)
         await ctx.message.delete()
-        fullArgs = " ".join(args)
-        if fullArgs == '':
-            with open('storage.json','r') as readfile:
-                indexFile = json.load(readfile)
-                for index in indexFile:
-                    if index['nick'] == ctx.message.author.name:
-                        if index['value'] == "":
-                            profileEmbed.add_field(name=f"__{index['nick']}'s Profile__",value="``**It's Empty**``")
-                        else:
-                            profileEmbed.add_field(name=f"__{index['nick']}'s Profile__",value=f"``{index['value']}``")
-                            
-                        await ctx.send(embed=profileEmbed)
-        else:
-            with open('storage.json','r') as readfile:
-                indexFile = json.load(readfile)
-                for index in indexFile:
-                    if index['nick'] == ctx.message.author.name:
-                        storeThis = [{"nick": ctx.message.author.name, "value": "{}".format(" ".join(args))}]
-                        with open('storage.json','w') as outfile:
-                            json.dump(storeThis, outfile)
-                    else:
-                        storeThis.append(storeThis)
-                        with open('storage.json','w') as outfile:
-                            json.dump(storeThis, outfile)
+        #Title of the Embed
+        profileEmbed = Embed(title="Profile",discription="Your Work Profile",color=0x56B9CD)
+        #just Making sure it can see all the Arguments
+        all_args = " ".join(args)
+        #if there is nothing in the args
+        if all_args == "":
+            #Checking the File Size
+            if os.stat("storage.json")[stat.ST_SIZE] == 0:
+                await ctx.send("This is The first time this command has been Run, Please run again to see profile!")
+                #Getting the Server and the Roles
+                guild = self.bot.get_guild(ctx.author.guild.id)
+                role = discord.utils.get(ctx.message.guild.roles, name='Soaring Heroes')
+                #Looking at all of the memebers
+                for member in guild.members:
+                    if role in member.roles and not member.bot:
+                        self.devList.append({'Discord Name': f"{member}", "nick": f"{member.nick}","description": "Nothing yet"})
+                #Puttting everything into a file
+                with open('storage.json','w') as outfile:
+                    json.dump(self.devList,outfile)
+                    outfile.close()
+            else:
+                #Finally displaying it all
+                with open('storage.json','r') as read:
+                    file = json.load(read)
+                    for user in file:
+                        if user['nick'] == ctx.message.author.nick:
+                            profileEmbed.add_field(name=f"**__{user['nick']}'s Profile__**", value=f"``{user['description']}``")
+                            profileEmbed.set_author(name=user['Discord Name'])
+                            await ctx.send(embed=profileEmbed)
+    
                         
+
+                        
+                
+
+            
+                        
+
+       
 
--- test.py
+++ test.py
@@ -14,13 +14,7 @@ class testCommand(commands.Cog):
     async def test(self,ctx):
         await ctx.message.delete()
         testEmbed = discord.Embed(title="Test Dialog", discription="For testing Stuff",color=0x56B9CD)
-        testEmbed.add_field(name="__What's Being Tested__", value="```Finding the Amount of Members```",inline=False )
-        testEmbed.add_field(name="*__Debug Log__*",value=f"```The Member list is below```",inline=False)
-        guild = self.bot.get_guild(ctx.author.guild.id)
-        role = discord.utils.find(lambda r: r.id == '955310316001046588',ctx.guild.roles)
-        for member in guild.members:
-            if role in member.roles:
-                testEmbed.add_field(name="Soaring Hero Devs", value=member)
+        testEmbed.add_field(name="__What's Being Tested__", value="```Finding the Amount of Members```",inline=False )     
 
             
         testEmbed.set_footer(text=ver)
